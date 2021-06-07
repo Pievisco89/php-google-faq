907aef9854f0158e4c5d84ea419be5a6ea4c6c10
Pievisco89 <pie-89@live.it>
2021-06-07 15:38:10 +0200

    Impostato css - inizio php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/style/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+  <title>Google FAQ</title>
+</head>
+<body>
+
+  <header>
+    <section class="header-top">
+      <div class="logo">
+        <img src="assets/img/googlelogo.png" alt="logo">
+        <a href="#">Privacy e termini</a>
+      </div>
+
+      <div class="user">
+        <i class="fas fa-bars"></i>
+        <img src="assets/img/user_img.png" alt="user">
+      </div>
+    </section>
+
+    <section class="header-bottom"> 
+      <ul>
+
+        <li>Introduzione</li>
+        <li>Norme sulla privacy</li>
+        <li>Termini di servizio</li>
+        <li>Tecnologie</li>
+        <li class="active">Domande frequenti</li>
+
+      </ul>
+    </section>
+
+  </header>
+
+  <main>
+
+    <?php
+
+    ?>
+
+  </main>
+
+  <footer>
+    <div class="footer-container">
+      <section class="footer-nav">
+        <ul>
+          <li>Google</li>
+          <li>Tutto su Google</li>
+          <li>Privacy</li>
+          <li>Termini</li>
+        </ul>
+      </section>
+
+      <section class="footer-lang">
+        <select name="Seleziona la lingua" id="">
+          <option value="">Italiano</option>
+          <option value="">English</option>
+          <option value="">Deutsch</option>
+        </select>
+      </section>
+    </div>
+
+  </footer>
+  
+</body>
+</html>
